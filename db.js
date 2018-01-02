aaff0728de50bca7f0bb88e1e256d8058114115a
Ikechi Michael <mykehell123@gmail.com>
2018-01-02 16:52:46 +0100

    feat: retry connection if mongoose fails on first try

--- db.js
+++ db.js
@@ -3,6 +3,14 @@ const logger = require('./logger')('db.js')
 
 mongoose.connection.on('error', (err) => {
     logger.error(err)
+
+    logger.warn('retrying in a second ...')
+
+    setTimeout(() => mongoose.connect(process.env.DB_URL, { useMongoClient: true }), 1000)
 })
 
-mongoose.connect(process.env.DB_URL, { useMongoClient: true });
+mongoose.connection.on('connected', () => {
+    logger.log('connected')
+})
+
+mongoose.connect(process.env.DB_URL, { useMongoClient: true })
