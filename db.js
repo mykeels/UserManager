60e43774329e5bbab4ed4a6b131b4fb398b3fb27
Ikechi Michael <mykehell123@gmail.com>
2018-01-02 15:09:48 +0100

    feat: detect and log mongoose connection issues

--- db.js
+++ db.js
@@ -1,2 +1,8 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const logger = require('./logger')('db.js')
+
+mongoose.connection.on('error', (err) => {
+    logger.error(err)
+})
+
 mongoose.connect(process.env.DB_URL, { useMongoClient: true });
